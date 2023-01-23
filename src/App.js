44f4a687e47a7822b290b1993a1598ee0e6e5936
IT20253912 <IT20253912@my.sliit.lk>
2023-01-23 20:24:01 +0530

    mao complete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 
 import './App.css';
 import Main from './Components/Main';
+import myData from './Data/myData';
 
-const myobj = [
-  {
-    name:'name1',
-    city:'city1',
-    position:'pos1'
-  },
-  {
-    name:'name2',
-    city:'city2',
-    position:'pos2'
-  },
-  {
-    name:'name3',
-    city:'city3',
-    position:'pos3'
-  },
-  {
-    name:'name4',
-    city:'city4',
-    position:'pos4'
-  }
-]
+
+const Test = myData.map(({id, name,city,position}) => {
+  return (
+    <Main key={id} name={name} city={city} position={position}/>  
+  );
+})
+
+
+
 
 function App() {
   return (
     <div>
       <h1>chathudu</h1>
       {/* <Main /> */}
-      {
-        myobj.map((ele) => {
-          return (
-            <Main name={ele.name} city={ele.city} position={ele.position}/>
-            
-            
-          );
-        })
-      }
-     
-      
+      {Test}
     </div>
   );
 }
---
+++ src/Data/myData.js
@@ -0,0 +1,28 @@
+const myData = [
+    {
+      id:'1',
+      name:'name1',
+      city:'city1',
+      position:'pos1'
+    },
+    {
+      id:'2',
+      name:'name2',
+      city:'city2',
+      position:'pos2'
+    },
+    {
+      id:'3',
+      name:'name3',
+      city:'city3',
+      position:'pos3'
+    },
+    {
+      id:'4',
+      name:'name4',
+      city:'city4',
+      position:'pos4'
+    }
+  ]
+
+  export default myData;
